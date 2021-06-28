13cf2204f2db9fd46e11654246ba265fe2ab1ba2
gr4yha7 <jamaal.ekpenyong22@gmail.com>
2021-06-28 14:12:16 +0100

    implemented frontend tasks

--- app/src/index.js
+++ app/src/index.js
@@ -41,7 +41,10 @@ const App = {
 
   // Implement Task 4 Modify the front end of the DAPP
   lookUp: async function (){
-    
+    const { lookUptokenIdToStarInfo } = this.meta.methods;
+    const starId = document.getElementById("lookid").value;
+    const starName = await lookUptokenIdToStarInfo(starId).call();
+    App.setStatus("Star Name is " + starName + ".");
   }
 
 };
